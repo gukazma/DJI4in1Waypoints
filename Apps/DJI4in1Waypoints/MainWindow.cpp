c414de774aff8a4c72b0b26bec529d1d19fc9750
gukazma <gukazma@proton.me>
2024-07-30 10:38:38 +0800

    ✨ feat: 初始化界面

--- Apps/DJI4in1Waypoints/MainWindow.cpp
+++ Apps/DJI4in1Waypoints/MainWindow.cpp
@@ -1,15 +1,22 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
-
+#include <QFileDialog>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->pushButton, &QPushButton::clicked, [&]() { 
+    connect(ui->toolButton, &QPushButton::clicked, [&]() {
+        auto result = QFileDialog::getOpenFileName(this, tr("Open input waypoints txt file"), "", "*.txt");
+        if (!result.isEmpty())
+        {
+            ui->lineEdit->setText(result);
+        }
+        });
+   /* connect(ui->pushButton, &QPushButton::clicked, [&]() { 
         ui->label->setText("2");
     });
-    connect(ui->pushButton_2, &QPushButton::clicked, [&]() { ui->label->setText("4"); });
+    connect(ui->pushButton_2, &QPushButton::clicked, [&]() { ui->label->setText("4"); });*/
 }
 
 MainWindow::~MainWindow()
--- Apps/DJI4in1Waypoints/main.cpp
+++ Apps/DJI4in1Waypoints/main.cpp
@@ -5,9 +5,6 @@
 int main(int argc, char** argv)
 {
     QApplication  app(argc, argv);
-    QPixmap       pixmap(QString::fromLocal8Bit(":/mgukazma.jpg"));
-    QSplashScreen splash(pixmap);
-    splash.show();
 
     QTranslator* translator = new QTranslator;
     translator->load(":/Translations/zh_CN.qm");
@@ -16,6 +13,5 @@ int main(int argc, char** argv)
     MainWindow mainwindow;
     mainwindow.showMaximized();
 
-    splash.finish(&mainwindow);
     return app.exec();
 }
